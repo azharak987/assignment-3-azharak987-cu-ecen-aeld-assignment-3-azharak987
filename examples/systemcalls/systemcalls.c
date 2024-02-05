98cd81d0a44b7f88b31af05e06970bfbace79ded
azharak987 <azhar987k@gmail.com>
2024-02-05 20:42:36 +0500

    Fixed SYSCAlls

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -7,11 +7,13 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>  // for open
 
 bool do_system(const char *cmd) {
     int ret = system(cmd);
     return WIFEXITED(ret) && WEXITSTATUS(ret) == 0;
 }
+
 bool do_exec(int count, ...) {
     va_list args;
     va_start(args, count);
@@ -33,6 +35,11 @@ bool do_exec(int count, ...) {
         perror("Fork failed");
         return false;
     } else if (pid == 0) {
+        // Close standard input, output, and error to prevent interference
+        close(STDIN_FILENO);
+        close(STDOUT_FILENO);
+        close(STDERR_FILENO);
+
         execv(command[0], command);
 
         // execv will only return if there's an error
@@ -46,7 +53,6 @@ bool do_exec(int count, ...) {
 
     va_end(args);
 }
-
 
 bool do_exec_redirect(const char *outputfile, int count, ...) {
     va_list args;
@@ -64,11 +70,18 @@ bool do_exec_redirect(const char *outputfile, int count, ...) {
         perror("Fork failed");
         return false;
     } else if (pid == 0) {
-        FILE *output = freopen(outputfile, "w", stdout);
-        if (output == NULL) {
-            perror("freopen failed");
+        int output_fd = open(outputfile, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+        if (output_fd == -1) {
+            perror("Open failed");
             exit(EXIT_FAILURE);
         }
+
+        // Redirect standard output to the output file
+        if (dup2(output_fd, STDOUT_FILENO) == -1) {
+            perror("Dup2 failed");
+            exit(EXIT_FAILURE);
+        }
+        close(output_fd);
 
         // If the command doesn't have an absolute path, use execvp
         if (command[0][0] != '/') {
